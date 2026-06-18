b34c3723f95fcf36293fedf422e4d5f73ecedd16
mariofriasc <mariofriasc@gmail.com>
2026-06-18 11:21:14 +0200

    Update Buscador.py

--- Buscador.py
+++ Buscador.py
@@ -1,4 +1,5 @@
 import chromadb
+import ollama
 
 # 1. Nos conectamos a la carpeta que ya tiene los datos guardados
 chroma_client = chromadb.PersistentClient(path="./cine_db")
@@ -13,9 +14,33 @@ resultados = collection.query(
     n_results=1
 )
 
-# 3. Mostramos el resultado
-for i in range(len(resultados['documents'][0])):
-    print(f"🎬 Película {i+1}:")
-    print(f"📄 Sinopsis: {resultados['documents'][0][i]}")
-    print(f"📋 Metadatos: {resultados['metadatas'][0][i]}")
-    print("-" * 50)
+sinopsis=resultados['documents'][0][0]
+titulo_encontrado=resultados['metadatas'][0][0]['titulo']
+anio_encontrado=resultados['metadatas'][0][0]['anio']
+
+print(f"✅ Película encontrada: '{titulo_encontrado}' ({anio_encontrado})")
+print(f"pasando información a ollama para generar un resumen de la sinopsis"  )
+prompt_sistema=("Eres un agente experto en cine. El usuario te pedirá una recomendación. "
+    "Debes usar ÚNICAMENTE la siguiente información de la película que encontramos en nuestra base de datos "
+    "para responderle de forma amable, entusiasta y completamente en español.\n\n"
+    f"Película: {titulo_encontrado} ({anio_encontrado})\n"
+    f"Sinopsis: {sinopsis}"
+)
+
+respuesta_ollama = ollama.chat(
+    model='llama3.2', 
+    messages=[
+        {'role': 'system', 'content': prompt_sistema},
+        {'role': 'user', 'content': consulta}
+    ]
+)
+# 5. Mostramos el resultado final en la pantalla
+print("================= RESPUESTA DEL AGENTE =================")
+# Intentamos con la estructura estándar de Ollama
+try:
+    print(respuesta_ollama['message']['content'])
+except (KeyError, TypeError):
+    # Si la librería se actualizó o cambia el formato, esto asegura que imprima igual
+    print(respuesta_ollama.message.content)
+print("========================================================")
+
